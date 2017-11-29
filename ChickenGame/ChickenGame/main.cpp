d312f2d771a60c472a94d0a5afb4e2a87fbf39c9
Unknown <cwhite788@gmail.com>
2017-11-29 12:24:41 +0000

    Added Copy of tutorial project with sfml engine
    Includes a copy of SFML linked to a Visual Studio Project. Contents of tutorial game will be replaced with new game content

---
+++ ChickenGame/ChickenGame/main.cpp
@@ -0,0 +1,16 @@
+#include "Application.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
+int main()
+{
+	try {
+		Application app;
+		app.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+}
